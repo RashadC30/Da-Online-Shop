6c0acce6b203d8f1824acb1c68ee555b0dd7145c
Rashad Clement <yosh3655@gmail.com>
2022-10-26 12:50:06 -0700

    Trying a new PORT

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ const notFoundHandler = require("./middlewares/not-found");
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
